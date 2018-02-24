5f4527a53520815bd8223a868ffbeab2c6ddb7af
prisng <priscillasrng@gmail.com>
2018-02-23 22:34:38 -0800

    Synced file system

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/11/10049dc52b1900181fb7e0de96ee3dda
@@ -0,0 +1,3 @@
+object dds {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+}
--- FunctionLab/.worksheet/src/session.scala
+++ FunctionLab/.worksheet/src/session.scala
@@ -1,3 +1,19 @@
 object session {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(60); 
   println("Welcome to the Scala worksheet")}
+
+  /********** #1 **********/
+  
+  
+  /********** #2 **********/
+  
+  
+  /********** #3 **********/
+  
+  
+  /********** #4 **********/
+  
+  
+  /********** #5 **********/
+  
+  
 }
