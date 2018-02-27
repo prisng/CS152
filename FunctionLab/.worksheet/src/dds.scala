4b622430ae4ca597b7642ee7f578266ccaf22972
prisng <priscillasrng@gmail.com>
2018-02-26 20:33:26 -0800

    Finished DDS. Working on combinators using recursion

--- FunctionLab/.worksheet/src/dds.scala
+++ FunctionLab/.worksheet/src/dds.scala
@@ -1,3 +1,88 @@
-object dds {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(56); 
-  println("Welcome to the Scala worksheet")}
+object dds {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(290); 
+
+  /********** #1 **********/
+  
+  // Tail-recursive implementation of controlLoop
+	def controlLoop[S](state: S, cycle: Int, halt: (S, Int) => Boolean, update: (S, Int) => S): S =
+		if (halt(state, cycle)) state
+		else controlLoop(update(state, cycle), cycle + 1, halt, update);System.out.println("""controlLoop: [S](state: S, cycle: Int, halt: (S, Int) => Boolean, update: (S, Int) => S)S""");$skip(356); 
+		
+  /********** #2 **********/
+  
+  // Initial pop = 1, doubles every week, stops after pop > 10^5, final pop = ?
+	def amoebaPop(): Int = {
+		val birthRate = 2
+		def updatePop(currentPop: Int, numWeeks: Int) = currentPop * birthRate
+		def halt(currentPop: Int, numWeeks: Int) = (currentPop > scala.math.pow(10, 5))
+		controlLoop(1, 0, halt, updatePop)
+	};System.out.println("""amoebaPop: ()Int""");$skip(28); 
+	val finalPop = amoebaPop();System.out.println("""finalPop  : Int = """ + $show(finalPop ));$skip(571); 
+	
+	// The final population size is 131072.
+	
+	
+  /********** #3 **********/
+  
+  	def solve(f: Double => Double): Double = {
+  		// Note: the smaller the delta, the longer it takes to run
+		val delta = 0.00001
+
+		// Derivative equation: except you get rid of the limit part (just an approximation)
+		def df(x: Double) = (f(x + delta) - f(x)) / delta
+		
+		def improveApprox(guess: Double, numGuesses: Int) =
+			guess - (f(guess) / df(guess))
+			
+		def halt(guess: Double, numGuesses: Int) =
+			math.abs(f(guess)) <= delta
+			
+		controlLoop(1.0, 0, halt, improveApprox)
+	};System.out.println("""solve: (f: Double => Double)Double""");$skip(109); 
+  
+  // --- Tester functions --- //
+  // x^2 - x - 6 = 0 => x = 3, x = -2
+  def f(x: Double) = x * x - x - 6;System.out.println("""f: (x: Double)Double""");$skip(78); 
+  // x^2 + 3x - 4 = 0 => x = 1, x = -4
+  def g(x: Double) = x * x	+ 3 * x - 4;System.out.println("""g: (x: Double)Double""");$skip(14); val res$0 = 
+  
+  solve(f);System.out.println("""res0: Double = """ + $show(res$0));$skip(11); val res$1 = 
+  solve(g);System.out.println("""res1: Double = """ + $show(res$1));$skip(121); 
+  
+  
+  /********** #4 **********/
+  
+  def squareRoot(x: Double) = {
+  		def f(y: Double) = y * y - x
+  		solve(f)
+  	};System.out.println("""squareRoot: (x: Double)Double""");$skip(19); val res$2 = 
+  
+  squareRoot(9);System.out.println("""res2: Double = """ + $show(res$2));$skip(17); val res$3 = 
+  squareRoot(16);System.out.println("""res3: Double = """ + $show(res$3));$skip(17); val res$4 = 
+  squareRoot(25);System.out.println("""res4: Double = """ + $show(res$4));$skip(122); 
+  
+  
+  /********** #5 **********/
+  
+  def cubeRoot(x: Double) = {
+  		def f(y: Double) = y * y * y - x
+  		solve(f)
+  };System.out.println("""cubeRoot: (x: Double)Double""");$skip(18); val res$5 = 
+  
+  cubeRoot(27);System.out.println("""res5: Double = """ + $show(res$5));$skip(15); val res$6 = 
+  cubeRoot(64);System.out.println("""res6: Double = """ + $show(res$6));$skip(16); val res$7 = 
+  cubeRoot(125);System.out.println("""res7: Double = """ + $show(res$7));$skip(134); 
+  
+  
+  /********** #6 **********/
+  
+  def nthRoot(x: Double, n: Int) = {
+  		def f(y: Double) = math.pow(y, n) - x
+  		solve(f)
+  };System.out.println("""nthRoot: (x: Double, n: Int)Double""");$skip(19); val res$8 = 
+  
+  nthRoot(9, 2);System.out.println("""res8: Double = """ + $show(res$8));$skip(17); val res$9 = 
+  nthRoot(27, 3);System.out.println("""res9: Double = """ + $show(res$9));$skip(16); val res$10 = 
+  nthRoot(4, 2);System.out.println("""res10: Double = """ + $show(res$10));$skip(17); val res$11 = 
+  nthRoot(16, 2);System.out.println("""res11: Double = """ + $show(res$11))}
+  
 }
--- FunctionLab/.worksheet/src/session.scala
+++ FunctionLab/.worksheet/src/session.scala
@@ -19,19 +19,97 @@ object session {;import org.scalaide.worksheet.runtime.library.WorksheetSupport.
 	
 	// Testing the composition function
 	squareTriple(2);System.out.println("""res2: Int = """ + $show(res$2));$skip(17); val res$3 = 
-	squareTriple(5);System.out.println("""res3: Int = """ + $show(res$3))}
+	squareTriple(5);System.out.println("""res3: Int = """ + $show(res$3));$skip(242); 
 	
   
   /********** #2 **********/
+  // f is composed with itself n times
+  def selfIter[T](f: T=>T, n: Int): T => T = {
+  		def id[T](x: T) = x
+  		if (n == 0) id
+  		else	 {
+  			def test = selfIter(f, n - 1)
+  			compose(f, test)
+  		}
+  };System.out.println("""selfIter: [T](f: T => T, n: Int)T => T""");$skip(65); 
   
-  
+  // --- Tester functions --- //
+  def inc(x: Double) = x + 1;System.out.println("""inc: (x: Double)Double""");$skip(31); 
+	def double(x: Double) = 2 * x;System.out.println("""double: (x: Double)Double""");$skip(76); 
+	
+	// Testing the selfIter function
+	val selfDouble = selfIter(double _, 3);System.out.println("""selfDouble  : Double => Double = """ + $show(selfDouble ));$skip(84); val res$4 = 
+	// double(double(double(5))) = double(double(10)) = double(20) = 40
+	selfDouble(5);System.out.println("""res4: Double = """ + $show(res$4));$skip(34); 
+	val selfInc = selfIter(inc _, 2);System.out.println("""selfInc  : Double => Double = """ + $show(selfInc ));$skip(41); val res$5 = 
+	// inc(inc(5)) = inc(6) = 7
+	selfInc(5);System.out.println("""res5: Double = """ + $show(res$5));$skip(63); 
+	val doubleIncSelfIter = selfIter(compose(double _, inc _), 3);System.out.println("""doubleIncSelfIter  : Double => Double = """ + $show(doubleIncSelfIter ));$skip(218); val res$6 = 
+	// double(inc(double(inc(double(inc(4))))))
+	// = double(inc(double(inc(double(5)))))
+	// = double(inc(double(inc(10))))
+	// = double(inc(double(11)))
+	// = double(inc(22))
+	// = double(23) = 46
+	doubleIncSelfIter(4);System.out.println("""res6: Double = """ + $show(res$6));$skip(273); 
+
   /********** #3 **********/
+  // how do i do this without iteration and with recursion instead
+  def countPass[T](values: Array[T], test: T => Boolean): Int = {
+  /*
+  		var count = 0
+		for(value <- values if (test(value) == true)) count = count + 1
+		count
+		*/
+		1
+  };System.out.println("""countPass: [T](values: Array[T], test: T => Boolean)Int""");$skip(67); 
   
+  // --- Tester functions --- //
+  def odd(n: Int) = n % 2 != 0;System.out.println("""odd: (n: Int)Boolean""");$skip(38); 
+  def pal(n: String) = n == n.reverse;System.out.println("""pal: (n: String)Boolean""");$skip(44); val res$7 = 
+  
+  countPass(Array(1, 2, 3, 4, 5), odd _);System.out.println("""res7: Int = """ + $show(res$7));$skip(48); val res$8 = 
+  countPass(Array(2, 5, 13, 17, 19, 20), odd _);System.out.println("""res8: Int = """ + $show(res$8));$skip(48); val res$9 = 
+
+  countPass(Array("mom", "dad", "dog"), pal _);System.out.println("""res9: Int = """ + $show(res$9));$skip(63); val res$10 = 
+  countPass(Array("racecar", "civic", "anna", "madam"), pal _);System.out.println("""res10: Int = """ + $show(res$10));$skip(418); 
   
   /********** #4 **********/
+  // this is also the iterative solution, not recursive
   
+  def recur(baseVal: Int, combiner: (Int, Int) => Int): Int => Int = {
+  		/*
+		def f(n: Int): Int = {
+ 			var result = baseVal
+ 			for (count <- 1 to n)
+ 				result = combiner(count, result)
+ 			result
+ 		}
+ 		f _
+ 		*/
+ 		def f(n: Int): Int = {
+			//if (baseVal == 0) 0
+ 			//else f(combiner(n, n - 1))
+ 			1
+ 		}
+ 		f _
+  };System.out.println("""recur: (baseVal: Int, combiner: (Int, Int) => Int)Int => Int""");$skip(72); 
+  
+  
+ 	// --- Tester functions --- //
+	val factorial = recur(1, _ * _);System.out.println("""factorial  : Int => Int = """ + $show(factorial ));$skip(47); 
+	val tri = recur(0, (n: Int, m: Int) => n + m);System.out.println("""tri  : Int => Int = """ + $show(tri ));$skip(16); val res$11 = 
+	
+	factorial(5);System.out.println("""res11: Int = """ + $show(res$11));$skip(14); val res$12 = 
+	factorial(4);System.out.println("""res12: Int = """ + $show(res$12));$skip(14); val res$13 = 
+	factorial(3);System.out.println("""res13: Int = """ + $show(res$13));$skip(10); val res$14 = 
+	
+	tri(5);System.out.println("""res14: Int = """ + $show(res$14));$skip(9); val res$15 = 
+ 	tri(4);System.out.println("""res15: Int = """ + $show(res$15));$skip(9); val res$16 = 
+ 	tri(3);System.out.println("""res16: Int = """ + $show(res$16));$skip(97); 
   
   /********** #5 **********/
   
+  def deOptionize[S, T](f: S => Option[T]): S => T = { null };System.out.println("""deOptionize: [S, T](f: S => Option[T])S => T""")}
   
 }
